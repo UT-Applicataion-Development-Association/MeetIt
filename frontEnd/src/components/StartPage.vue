a1208746b7922588daebb1f1bffc8f418637cb63
nzhou27 <zhouhanzhi0818@gmail.com>
2022-07-17 01:44:18 -0400

    starter page draft, uploaded background picture to /public

--- frontEnd/src/components/StartPage.vue
+++ frontEnd/src/components/StartPage.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Start Page</h1>
+  <div class="background">
+    <button id="b"><b>Start</b></button>
+
+
+  </div>
+  
+
+  
 </template>
+
+
+<script>
+    export default {
+      name: "StartPage"
+      
+    }
+
+</script>
+
+<style>
+
+html { 
+  background: url(/public/starterBg.png) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+#b{
+  background-color:white;
+  border-radius: 35px;
+  position: absolute;
+  left: 43%;
+  top: 80%;
+  padding: 10px 120px;
+  border: none;
+  font-size: 25px;
+} 
+
+</style>
